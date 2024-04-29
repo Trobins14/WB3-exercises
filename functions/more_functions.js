21622e87a0a484a7ce0ce99f005a6302d4e10900
trobins14 <tristan.robins14@gmail.com>
2024-04-29 11:45:00 -0400

    created base variables

---
+++ functions/more_functions.js
@@ -0,0 +1,25 @@
+// more functions
+
+let name = "Tristan Robins";
+let address = "101 Scarbrush Ct.";
+let city = "Stockbridge";
+let state = "GA";
+let zip = "30281";
+
+displayMailingLabel(name, address, city, state, zip){
+
+};
+
+let num1;
+let num2;
+
+addNumbers(num1, num2){
+
+};
+
+let totalDue;
+let amountPaid;
+
+displayReceipt(totalDue, amountPaid){
+
+};
