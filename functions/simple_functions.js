0429bae6fc0472c44ed0cadc02dbdb86f27a5a0c
trobins14 <tristan.robins14@gmail.com>
2024-04-29 10:45:02 -0400

    Created a simple functions page. functions created work correctly.

---
+++ functions/simple_functions.js
@@ -0,0 +1,20 @@
+//simple functions exercise
+let name = "Tristan";
+let favoriteMovie = "Head of State";
+let facoriteMusician = "Dave East";
+
+function favoriteThings(){
+    console.log(`My name is ${name}. My favorite movie is ${favoriteMovie}, and my favorite musician is ${facoriteMusician}.`);
+};
+
+function whyImHere(){
+    console.log(`I'm here because I want to start a career path with a higher salary and expertise.`)
+};
+
+function favoritePlace(){
+    console.log(`My favorite place is home when my family is around.`)
+};
+
+favoriteThings();
+whyImHere();
+favoritePlace();
