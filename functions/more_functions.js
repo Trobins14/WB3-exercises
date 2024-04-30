4e808c88c0b01141bf91ffee4a41ae99ce1e32da
trobins14 <tristan.robins14@gmail.com>
2024-04-29 20:52:06 -0400

    began to create functions that are required.

--- functions/more_functions.js
+++ functions/more_functions.js
@@ -7,19 +7,19 @@ let state = "GA";
 let zip = "30281";
 
 displayMailingLabel(name, address, city, state, zip){
-
+    console.log(`${name}, ${address} ${city}, ${state} ${zip}`)
 };
 
-let num1;
-let num2;
+let num1 = 7;
+let num2 = 42;
 
 addNumbers(num1, num2){
-
+    console.log(num1 + num2);
 };
 
 let totalDue;
 let amountPaid;
 
 displayReceipt(totalDue, amountPaid){
-
+    
 };
